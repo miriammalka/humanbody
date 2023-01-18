ef1a7b137e54e4ba240ec85345ff221d984324c8
miriammalka <miriammalka1234@gmail.com>
2023-01-18 16:46:03 -0700

    added smaple data

--- 1 table.sql
+++ 1 table.sql
@@ -1,3 +1,5 @@
 use humanbodyDB
 go 
-create table dbo.humanbody(id int not null identity primary key)
+create table dbo.humanbody(id int not null identity primary key, 
+bodypart varchar(100) not null, 
+num int not null)
