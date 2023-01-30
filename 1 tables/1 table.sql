c0621fd70e302c35b057789d22e12e8d50c9fe42
miriammalka <miriammalka1234@gmail.com>
2023-01-30 16:53:46 -0700

    made the num column nullable

--- 1 tables/1 table.sql
+++ 1 tables/1 table.sql
@@ -1,5 +1,7 @@
 use humanbodyDB
 go 
-create table dbo.humanbody(id int not null identity primary key, 
+drop table if exists body
+go
+create table dbo.body(id int not null identity primary key, 
 bodypart varchar(100) not null, 
-num int not null)
+num int null)
--- 2 sample data/2 sample data.sql
+++ 2 sample data/2 sample data.sql
@@ -1,7 +1,11 @@
-insert humanbody(bodypart, num)
+insert body(bodypart, num)
 select 'eyes', 2
 union select 'ears', 2
 union select 'nose', 1
 union select 'mouth', 2
 union select 'teeth', 32
 union select 'toungue', 1
+
+--how do you store body parts that don't have a fixed number
+insert body(bodypart)
+select 'hair'
