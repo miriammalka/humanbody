4b1995debe43ff9d7cd8be1c04770a79f51e6f57
miriammalka <97982214+miriammalka@users.noreply.github.com>
2023-01-30 17:26:38 -0700

    Update 1 table.sql
    added planet to the body table

--- 1 tables/1 table.sql
+++ 1 tables/1 table.sql
@@ -2,6 +2,7 @@ use humanbodyDB
 go 
 drop table if exists body
 go
-create table dbo.body(id int not null identity primary key, 
+create table dbo.body(id int not null identity primary key,
+planet varchar(50) not null,
 bodypart varchar(100) not null, 
 num int null)
